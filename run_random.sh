c2bb05b3cf0769804dd535b86f4bfb4a9d0407c5
Today <ipp.10032018@gmail.com>
2022-04-10 20:41:54 +0900

    [update] change output of result

--- run_random.sh
+++ run_random.sh
@@ -9,29 +9,29 @@ function terminate_execution () {
 
 # Check necessary files.
 flag=false
-if [ ! -e "a.out" ]; then
+if [[ ! -e "a.out" ]]; then
     echo "\"a.out\" does not exist"
     flag=true
 fi
 
-if [ ! -e "b.out" ]; then
+if [[ ! -e "b.out" ]]; then
     echo "\"b.out\" does not exist"
     flag=true
 fi
 
-if [ ! -e "make_random.cpp" ]; then
+if [[ ! -e "make_random.cpp" ]]; then
     echo "\"make_random.cpp\" does not exist"
     flag=true
 fi
 
-if "${flag}"; then
+if "$flag"; then
     terminate_execution
 fi
 
 
 # Compile "make_random.cpp".
 g++ "make_random.cpp" -o "c.out"
-if [ ! "$?" == 0 ]; then
+if (( "$?" != 0 )); then
     echo "Fail in compiling make_random.cpp"
     terminate_execution
 fi
@@ -43,13 +43,23 @@ while true; do
     ans1=$(./a.out < input.txt)
     ans2=$(./b.out < input.txt)
     
-    if [ "$ans1" != "$ans2" ]; then
-        printf "Wrong Answer!\n\n"
-        printf "[IN]\n"
-        cat input.txt
-        printf "\n"
-        printf "[OUT a]\n${ans1}\n\n"
-        printf "[OUT b]\n${ans2}\n"
+    if [[ "$ans1" != "$ans2" ]]; then
+        echo "Wrong Answer!"
+        echo ""
+        
+        if (( $(wc -c < input.txt) <= 10000 )); then
+            echo "[IN]"
+            cat input.txt
+            echo ""
+        fi
+        
+        echo "[OUT a]"
+        echo "${ans1}"
+        echo ""
+        echo "[OUT b]"
+        echo "${ans2}"
         exit
     fi
 done
+
+# Do not reach.

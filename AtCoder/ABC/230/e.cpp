184d2727cc449507d9da3b16e0c2c922ade7416f
Today <ipp.30042022@gmail.com>
2022-05-06 14:31:07 +0900

    [add] create a file "AtCoder/ABC/230/e.cpp"

---
+++ AtCoder/ABC/230/e.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define REP(i,n)   for(int i=0;i<(int)(n);++i)
+#define FOR(i,l,r) for(int i=(int)(l);i<(int)(r);++i)
+#define ALL(v)     (v).begin(),(v).end()
+#define debug(x)   std::cerr<<"(L"<<__LINE__<<") "<<#x<<": "<<(x)<<std::endl
+using ll  = long long;
+using vi  = std::vector<int>;
+using vvi = std::vector<vi>;
+using pii = std::pair<int,int>;
+using namespace std;
+constexpr int       INF  = 1e9;
+constexpr long long LINF = 1e18;
+constexpr double    EPS  = 1e-10;
+constexpr int       MOD  = 1e9+7;
+template<class Class> void line(const Class &a,std::ostream &os=std::cerr){int cnt=0;for(const auto &elem:a)os<<(cnt++==0?"":" ")<<elem;os<<std::endl;}
+
+int main(){
+    ll n;
+    cin>>n;
+    
+    ll k=sqrt(n);
+    ll ans=0;
+    for(ll i=1;i<=n/(k+1);++i) ans+=n/i;
+    for(ll i=1;i<=k;++i) ans+=(n/i-n/(i+1))*i;
+    
+    cout<<ans<<endl;
+}
+
+// I refered explanations.

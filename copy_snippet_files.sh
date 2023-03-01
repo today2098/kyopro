e5ad6fc00640ef9171fa3faf36f25856285f5f9f
Today <ipp.15122098@gmail.com>
2023-03-01 14:59:39 +0900

    [update] Copy a file for debug

--- copy_snippet_files.sh
+++ copy_snippet_files.sh
@@ -1,4 +1,6 @@
 echo 'copy these snippet files'
 ls -a -1 ~/prog/algorithm/.vscode/*.code-snippets
+ls -1 ~/prog/algorithm/src/Utils/debug.hpp
 
 cp ~/prog/algorithm/.vscode/*.code-snippets .vscode/
+cp ~/prog/algorithm/src/Utils/debug.hpp .

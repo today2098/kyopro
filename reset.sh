f4c78299d821c21aca802649739d8381c17d72f6
Today <76273733+today2098@users.noreply.github.com>
2024-06-30 02:56:17 +0900

    [update] Assume go files unchanged

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '> git update-index --no-assume-unchanged ./*.cpp ./input.txt ./output.txt'
-git update-index --no-assume-unchanged ./*.cpp ./input.txt ./output.txt
+echo '> git update-index --no-assume-unchanged ./*.cpp ./*.go ./input.txt ./output.txt'
+git update-index --no-assume-unchanged ./*.cpp ./*.go ./input.txt ./output.txt
 
 echo "complete resetting"

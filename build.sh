32f24612cd9d7b2e060977aa8613a5940233f47a
Today <76273733+today2098@users.noreply.github.com>
2024-06-30 02:19:38 +0900

    [add] Copy snippet files from my library

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 echo '> git update-index --assume-unchanged ./*.cpp'
 git update-index --assume-unchanged ./*.cpp
 
+echo '> bash ./copy_snippet_files.sh'
+bash ./copy_snippet_files.sh
+
 echo 'complete building'

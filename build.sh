eafec1ab59ca3632adcd4a58d6ae992d9735e89f
Today <76273733+today2098@users.noreply.github.com>
2024-07-13 19:13:55 +0900

    [update] Update two scripts to handle tmp files

--- build.sh
+++ build.sh
@@ -7,4 +7,7 @@ git update-index --assume-unchanged \
     *.cpp \
     input.txt output.txt
 
+echo '> ./copy_snippet_files.sh'
+./copy_snippet_files.sh
+
 echo 'complete building'
--- reset.sh
+++ reset.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo '> rm .vscode/*.code-snippets' \
+    'debug.hpp' \
+    '*.out'
+rm .vscode/*.code-snippets \
+    debug.hpp \
+    *.out
+
 echo '> git update-index --no-assume-unchanged' \
     '*.cpp' \
     'input.txt output.txt'
